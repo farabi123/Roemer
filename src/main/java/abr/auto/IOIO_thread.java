e05196e9d7f0b7dcd69dd7300c03b1db2c83c502
autumn <autumnl@uci.edu>
2018-01-29 15:26:57 -0800

    First Commit - Autumn

--- src/main/java/abr/auto/IOIO_thread.java
+++ src/main/java/abr/auto/IOIO_thread.java
@@ -3,35 +3,59 @@ package abr.auto;
 import ioio.lib.api.AnalogInput;
 import ioio.lib.api.DigitalOutput;
 import ioio.lib.api.PwmOutput;
+import ioio.lib.api.Uart;
 import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.BaseIOIOLooper;
 
-import android.util.Log;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import android.util.Log;
+import ioio.lib.api.AnalogInput;
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.Uart;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.BaseIOIOLooper;
 
 public class IOIO_thread extends BaseIOIOLooper
 {
-    static final int DEFAULT_PWM = 1500, MAX_PWM = 2000, MIN_PWM = 1000;
+    //IR sensor
     AnalogInput  irCenter;
-    private PwmOutput pwm_speed_output, pwm_steering_output;
-
-    int pwm_speed, pwm_steering;
     float irCenterReading;
     float dataIRCenter;
+
+    //Servos
+    private PwmOutput pwm_pan_output, pwm_tilt_output;
+    InputStream in;
+    OutputStream out;
+    static final int DEFAULT_PWM = 1500, MAX_PWM = 2000, MIN_PWM = 1000;
+    int counter;
+
 
     @Override
     public void setup() throws ConnectionLostException
     {
-        try
-        {
+        try {
+            //IR Sensor
             irCenter = ioio_.openAnalogInput(43);
-            pwm_speed_output = ioio_.openPwmOutput(3, 50); //motor channel 4: front left
-            pwm_steering_output = ioio_.openPwmOutput(4, 50); //motor channel 3: back left
-
-            pwm_speed_output.setPulseWidth(1500);
-            pwm_steering_output.setPulseWidth(1500);
 
 
+            //Servos
+            pwm_pan_output = ioio_.openPwmOutput(5, 50); //motor channel 1: back right;
+            pwm_tilt_output = ioio_.openPwmOutput(6, 50); //motor channel 1: back right;
+
+            Uart uart = ioio_.openUart(3, 4, 230400, Uart.Parity.NONE, Uart.StopBits.ONE);
+            in = uart.getInputStream();
+            out = uart.getOutputStream();
+            counter++;
+            pwm_pan_output.setPulseWidth(DEFAULT_PWM);
+            pwm_tilt_output.setPulseWidth(DEFAULT_PWM);
         }
         catch (ConnectionLostException e){throw e;}
     }
@@ -39,27 +63,27 @@ public class IOIO_thread extends BaseIOIOLooper
     @Override
     public void loop() throws ConnectionLostException
     {
-        System.out.println("object HELLOOOOOO");
         ioio_.beginBatch();
+        try {
+            //IR Sensor
 
-            try {
-                pwm_speed_output.setPulseWidth(1800);
-                pwm_steering_output.setPulseWidth(1800);
-                //set_speed(1800);
-                dataIRCenter = irCenter.read();
-                System.out.println("object dataIRCenter" +dataIRCenter);
+            dataIRCenter = irCenter.read();
+            System.out.println("object dataIRCenter" + dataIRCenter);
 
-                   if (dataIRCenter > 0.3){
-                       System.out.println("object detected!!!!!!!!!!!!!!");
-                   }
-                    //irCenterReading = irCenter.getVoltage();
-
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                ioio_.disconnect();
-            } finally {
-                ioio_.endBatch();
+            if (dataIRCenter > 0.3){
+                System.out.println("object detected!!!!!!!!!!!!!!");
             }
+            //irCenterReading = irCenter.getVoltage();
+            pan(DEFAULT_PWM);
+            tilt(DEFAULT_PWM);
+            System.out.println("Counter: ");
+
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            ioio_.disconnect();
+        } finally {
+            ioio_.endBatch();
+        }
     }
 
 
@@ -67,36 +91,33 @@ public class IOIO_thread extends BaseIOIOLooper
         return dataIRCenter/*irCenterReading*/;
     }
 
-    public synchronized void set_speed(int value)
+    public synchronized void pan(int value)
     {
         try {
             if(value > MAX_PWM)
-                pwm_speed_output.setPulseWidth(MAX_PWM);
+                pwm_pan_output.setPulseWidth(DEFAULT_PWM);
             else if(value < MIN_PWM)
-                pwm_speed_output.setPulseWidth(MIN_PWM);
-            else {
-                if (pwm_speed_output != null) {
-                    pwm_speed_output.setPulseWidth(value);
-                }
-            }
+                pwm_pan_output.setPulseWidth(DEFAULT_PWM);
+            else
+                pwm_pan_output.setPulseWidth(value);
+        } catch (ConnectionLostException e) {
+            ioio_.disconnect();
+        }
+
+    }
+
+    public synchronized void tilt(int value)
+    {
+        try {
+            if(value > MAX_PWM)
+                pwm_tilt_output.setPulseWidth(DEFAULT_PWM);
+            else if(value < MIN_PWM)
+                pwm_tilt_output.setPulseWidth(DEFAULT_PWM);
+            else
+                pwm_tilt_output.setPulseWidth(value);
         } catch (ConnectionLostException e) {
             ioio_.disconnect();
         }
     }
 
-    public synchronized void set_steering(int value){
-        try {
-            if(value > MAX_PWM)
-                pwm_steering_output.setPulseWidth(MAX_PWM);
-            else if(value < MIN_PWM)
-                pwm_steering_output.setPulseWidth(MIN_PWM);
-            else {
-                if (pwm_steering_output != null) {
-                    pwm_steering_output.setPulseWidth(value);
-                }
-            }
-        } catch (ConnectionLostException e) {
-            ioio_.disconnect();
-        }
-    }
 }
